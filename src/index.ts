4fc025cfbef57647b58fb51da44cd02c33e65db1
Peter Caisse <pcaisse@gmail.com>
2019-08-29 14:16:58 -0400

    Exhaustive checks

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,37 @@ const formatMessage = (msg: Message) =>
   msg.yell ? msg.text.toUpperCase() : msg.text;
 
 console.log(formatMessage({ yell: true, text: "Hello!" }));
+
+interface Square {
+  kind: "square";
+  size: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+type Shape = Square | Rectangle | Circle;
+
+function area(s: Shape) {
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.width * s.height;
+    case "circle":
+      return Math.PI * s.radius * s.radius;
+    default:
+      const _exhaustiveCheck: never = s;
+      return _exhaustiveCheck;
+  }
+}
+
+console.log(area({ kind: "circle", radius: 4 }));
